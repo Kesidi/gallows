aea5767a2ad132d993f0b5eba8353767ddcabfcc
Vladimir Demchenko <demchenkovn1@gmail.com>
2013-07-06 17:50:48 +0300

    last test

--- lib/gallows/game.rb
+++ lib/gallows/game.rb
@@ -1,21 +1,21 @@
 module Gallows
   class Game
-    attr_accessor :secret_word, :public_str
-    def start
-      puts "hi"
-    end
-    def initialize(output, input)
+    def initialize(output=$stdout, input=$stdin)
       @output = output
       @input = input
-      @words = ["виселица","пшеница","ромашка","двигатель","гороскоп","ароматизатор"]
       @errors = 0
-      @tries = 0
-      @win = false
     end
-    def set_rand_word
-      @secret_word = @words.sample
+    def init
+      @secret_word = set_rand_word
+      @errors = 0
+      @win = false
       @public_str = "-"*@secret_word.length
     end
+
+    def set_rand_word
+      File.readlines("words.txt").sample
+    end
+
     def print_result
       @output.puts "----------"
       @output.puts "|        |"
@@ -32,25 +32,29 @@ module Gallows
       @output.puts "count errors - #{@errors}" 
     end
     def check_letter(char)
-      @tries += 1
       @secret_word.length.times { |i| @public_str[i] = char if (@secret_word[i] == char)}
       @secret_word.include?(char)
     end
+    def procces
+      @output.puts "Please enter the letter"
+      char = @input.gets.chomp
+      @errors += 1 unless check_letter(char[0])
+      print_result
+      @public_str.include?("-") ? @output.puts(@public_str) : @win = true
+      procces if continue_game
+    end
+
+    def continue_game
+      (@errors < 6) && (@win == false)
+    end
+
     def run
-      initialize($stdout, $stdin)
-      set_rand_word
-      loop do
-        @output.puts "Please enter the letter"
-        char = @input.gets
-        @errors += 1 unless check_letter(char[0])
-        print_result
-        @public_str.include?("-") ? @output.puts(@public_str) : @win = true
-        break if ((@errors >= 6) || (@win)) 
-      end
+      init
+      procces
       @win ? @output.puts("Congratulations you WIN!!!!! secret word is #{@public_str}") : @output.puts("You Lose! secret word is #{@secret_word}")
       @output.puts "Do you want to play again?(y/n)"
-      answer = @input.gets
-      run if (answer[0] == "y")
+      answer = @input.gets.chomp
+      run if (answer == "y")
     end
   end
-end
+end 
--- spec/gallows/game_spec.rb
+++ spec/gallows/game_spec.rb
@@ -5,33 +5,83 @@ module Gallows
     let(:output) { double('output').as_null_object }
     let(:input) { double('input').as_null_object }
     let(:game) { Game.new(output, input) }
-    context "start" do
-      it "should say hi" do
-        game.should_receive(:puts).with(/hi/i)
-        game.start
-      end
-    end
     describe "#set_rand_word" do
       it "set the secret word" do
-        game.set_rand_word
-        game.secret_word.length.should_not be_nil
-      end
-      it "return public str" do
-        game.set_rand_word.should == "-"*game.secret_word.length
+        game.set_rand_word.should_not be_nil
       end
     end
     describe "#check_letter" do
+      before(:each) do
+        game.stub(:set_rand_word).and_return("виселица")
+        game.init
+      end
       it "return false value" do
-        game.set_rand_word
-        game.check_letter("a").class.should == FalseClass
+        game.check_letter("j").should == false
       end
       it "return true value" do
-        game.secret_word = "виселица"
-        game.public_str = "--------"
-        game.check_letter("а").class.should == TrueClass
-
+        game.check_letter("а").should == true
       end
 
+    end
+    describe "#procces" do
+      before(:each) do
+        #game.stub(:secret_word).and_return("nice")
+        game.instance_variable_set(:@secret_word,"nice")
+        game.instance_variable_set(:@errors,5)          
+        #game.stub(:continue_game).and_return(false)
+        
+      end
+      it "prompts please enter letter" do
+        input.stub(:gets).and_return("j")
+        game.init
+        game.stub(:continue_game).and_return(false)
+        output.should_receive(:puts).with('Please enter the letter')
+        game.procces
+      end
+      it "open letter in public str" do
+        
+        game.instance_variable_set(:@public_str,"----")
+        #game.stub(:continue_game).and_return(false)
+        game.stub(:continue_game).and_return(false)
+        input.stub(:gets).and_return("i")
+        game.procces
+        game.instance_variable_get(:@public_str).should == "-i--"
+      end
+      it "dont open letter in public str" do
+        game.instance_variable_set(:@public_str,"----")
+        input.stub(:gets).and_return("h")
+        game.procces
+        game.instance_variable_get(:@public_str).should == "----"
+      end
+      it "set @win true" do
+        game.instance_variable_set(:@public_str,"n-ce")
+        game.stub(:continue_game).and_return(false)
+        input.stub(:gets).and_return("i")
+        game.procces
+        game.instance_variable_get(:@win).should == true
+      end
+    end
+    describe "#run" do
+      it "prompts win" do
+        game.stub(:set_rand_word).and_return("vice")
+        #game.instance_variable_set(:@secret_word,"vice")
+        #game.instance_variable_set(:@errors,5)
+        input.stub(:gets).and_return("v","i","c","e","n")
+        var = game.instance_variable_get(:@public_str )
+        output.should_receive(:puts).with(/Congratulations you WIN!!!!! secret word is #{var}/)
+        game.run
+
+      end
+      it "prompts lose" do
+        game.stub(:set_rand_word).and_return("vice")
+        #game.instance_variable_set(:@secret_word,"nice")
+        game.instance_variable_set(:@errors,5)
+        game.instance_variable_set(:@public_str,"v-ce")
+        input.stub(:gets).and_return("n")
+        var = game.instance_variable_get(:@secret_word)
+        output.should_receive(:puts).with(/You Lose! secret word is #{var}/)
+        game.run
+      end
     end
   end
 end
